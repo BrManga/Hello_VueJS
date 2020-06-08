68afc6794b346f73ee81e94f0b07d08af475ecf8
Bora Manga <bora.mng525@gmail.com>
2020-06-08 22:08:15 +0200

    V-FOR

--- Vue_Conditions&Listing/app.js
+++ Vue_Conditions&Listing/app.js
@@ -3,6 +3,11 @@ new Vue({
     data: {
         show: true,
         number: 10,
-        showParag: false
+        showParag: false,
+        products: ["PC", "Laptop", "Monitor"],
+        persons: [
+            { name: "Alex De Sousa", email: "alex@gmail.com" },
+            { name: "Fredo Giovanni", email: "fgiovanni@gmail.com" }
+        ]
     }
 })
