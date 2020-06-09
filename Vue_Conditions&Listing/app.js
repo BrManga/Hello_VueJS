4cd9d27b6c3b19c0ff0b5425c93e6d78d0707a9d
Bora Manga <bora.mng525@gmail.com>
2020-06-09 22:10:50 +0200

    Using Ref, template, , simple Compenent

--- Vue_Conditions&Listing/app.js
+++ Vue_Conditions&Listing/app.js
@@ -1,4 +1,8 @@
-new Vue({
+/* Simple Component */
+Vue.component('hello', {
+    template: '<h1>Hello from simple Component</h1>'
+})
+var app = new Vue({
     el: "#app",
     data: {
         show: true,
@@ -11,3 +15,13 @@ new Vue({
         ]
     }
 })
+/* Reaching to ref */
+setTimeout(() => {
+    /* What important here is: It won't be in VueJS Instance so watcher layer won't look at it */
+    app.$refs.MyButton.innerText = "Later Changed"
+}, 3000);
+/* Using String Template and Mount Together */
+var app2 = new Vue({
+    template: '<h1>I am coming from template in app.js</h1>'
+})
+app2.$mount(".forTemplate")
